7b60b99d5f5d84ec364d3a2b395bdac226b6e85c
mrkburgers <tareksalehphoto@outlook.com>
2026-09-13 06:16:33 +0000

    Add Socket.IO server foundation

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 const db = require("./database");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 app.use(express.json());
 const PORT = 3000;
 
