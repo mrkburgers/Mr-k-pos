913d43924d85d6920ea1bcd21ff62c22e9083239
mrkburgers <tareksalehphoto@outlook.com>
2026-09-15 20:22:18 +0000

    Load cashier combo quantity selector

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
   const indexPath = path.join(__dirname, "index.html");
   const html = fs.readFileSync(indexPath, "utf8").replace(
     "</body>",
-    '<script src="/kitchen-v2.js"></script>\n<script src="/menu-v2.js"></script>\n<script src="/menu-admin-v2.js"></script>\n<script src="/menu-recipe-v2.js"></script>\n<script src="/inventory-legacy-v2.js"></script>\n<script src="/inventory-delivery-v2.js"></script>\n<script src="/cashier-v2.js"></script>\n<script src="/sales-v2.js"></script>\n<script src="/shift-v2.js"></script>\n<script src="/owner-accounts-v2.js"></script>\n<script src="/expenses-v2.js"></script>\n<script src="/auth-v2.js"></script>\n</body>'
+    '<script src="/kitchen-v2.js"></script>\n<script src="/menu-v2.js"></script>\n<script src="/menu-admin-v2.js"></script>\n<script src="/menu-recipe-v2.js"></script>\n<script src="/inventory-legacy-v2.js"></script>\n<script src="/inventory-delivery-v2.js"></script>\n<script src="/cashier-v2.js"></script>\n<script src="/combo-quantity-v2.js"></script>\n<script src="/sales-v2.js"></script>\n<script src="/shift-v2.js"></script>\n<script src="/owner-accounts-v2.js"></script>\n<script src="/expenses-v2.js"></script>\n<script src="/auth-v2.js"></script>\n</body>'
   );
   res.type("html").send(html);
 });
@@ -59,6 +59,9 @@ app.get("/inventory-delivery-v2.js", (req, res) => {
 });
 app.get("/cashier-v2.js", (req, res) => {
   res.sendFile(path.join(__dirname, "cashier-v2.js"));
+});
+app.get("/combo-quantity-v2.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "combo-quantity-v2.js"));
 });
 app.get("/sales-v2.js", (req, res) => {
   res.sendFile(path.join(__dirname, "sales-v2.js"));
