a0db8b56ac55fbcd95a6858694b671cb6c465f26
mrkburgers <tareksalehphoto@outlook.com>
2026-09-17 19:47:38 +0000

    Register V2 staff employee backend

--- owner-accounts-api-v2.js
+++ owner-accounts-api-v2.js
@@ -1,9 +1,11 @@
 const createSecurityAuthV2=require("./security-auth-v2");
 const registerStaffPinV2=require("./staff-pin-api-v2");
+const registerStaffEmployeesV2=require("./staff-employees-api-v2");
 
 module.exports=function registerOwnerAccountsV2(app,io,db){
  const ownerOnly=createSecurityAuthV2().requireRole("owner");
  registerStaffPinV2(app,db);
+ registerStaffEmployeesV2(app,db);
 
  db.exec(`
   CREATE TABLE IF NOT EXISTS owner_accounts (
